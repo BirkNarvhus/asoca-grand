dde9a7341e4f6a364680420fd04d449ea1ef3506
Birk narvhus <birkon@ntnu.no>
2024-04-26 02:54:17 +0200

    added more tests

--- test_data.py
+++ test_data.py
@@ -4,7 +4,7 @@ from glob import glob
 import monai.bundle.utils
 import numpy as np
 from monai.transforms import Compose, LoadImageD, EnsureChannelFirstd, EnsureTyped, CenterSpatialCropD, \
-    RandSpatialCropD, RandScaleIntensityd, NormalizeIntensityd, RandShiftIntensityd, ToTensorD
+    RandSpatialCropD, RandScaleIntensityd, NormalizeIntensityd, RandShiftIntensityd, ToTensorD, ResizeD
 
 # In[ ]:
 
@@ -36,20 +36,15 @@ path_to_masks = get_paths(config_dict['dataset']['mask_path'])
 data = [{'image': image, 'mask': mask} for image, mask in zip(path_to_image, path_to_masks)]
 
 
-train_transforms = Compose([
-    LoadImageD(keys=["image", "mask"], reader="itkreader"),
-    EnsureChannelFirstd(keys=["image", "mask"]),
-    EnsureTyped(keys=["image", "mask"]),
-    ToTensorD(keys=["image", "mask"]),
-])
 
-'''
+
 train_transforms = Compose([
     LoadImageD(keys=["image", "mask"], reader="itkreader"),
     EnsureChannelFirstd(keys=["image", "mask"]),
     EnsureTyped(keys=["image", "mask"]),
-    CenterSpatialCropD(keys=["image", "mask"], roi_size=[400, 400, 100]),
-    RandSpatialCropD(keys=["image", "mask"], roi_size=[256, 256, 80], random_size=False),
+    CenterSpatialCropD(keys=["image", "mask"], roi_size=[400, 400, 200]),
+    RandSpatialCropD(keys=["image", "mask"], roi_size=[350, 350, 180], random_size=False),
+    ResizeD(keys=["image", "mask"], spatial_size=[256, 256, 112]),
     NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
     RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
     RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
@@ -61,33 +56,19 @@ val_transform = Compose(
         LoadImageD(keys=["image", "mask"], reader="itkreader"),
         EnsureChannelFirstd(keys=["image", "mask"]),
         EnsureTyped(keys=["image", "mask"]),
-        CenterSpatialCropD(keys=["image", "mask"], roi_size=[256, 256, 80]),
+        CenterSpatialCropD(keys=["image", "mask"], roi_size=[350, 350, 180]),
+        ResizeD(keys=["image", "mask"], spatial_size=[256, 256, 112]),
         NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
         ToTensorD(keys=["image", "mask"]),
     ]
 )
 
-'''
+train_set = monai.data.CacheDataset(data[:int(len(data) * 0.8)], transform=train_transforms) if config_dict['dataset'][
+    'cache_dataset'] else monai.data.Dataset(data[:int(len(data) * 0.8)], transform=train_transforms)
 
-train_set = monai.data.CacheDataset(data[:int(len(data))], transform=train_transforms) if config_dict['dataset'][
-    'cache_dataset'] else monai.data.Dataset(data[:int(len(data))], transform=train_transforms)
-
-
-print(f"Train set size: {len(train_set)}")
-
-
-for i in range(10):
-    item = train_set.__getitem__(i)
-
-    print(item["image"].shape, item["mask"].shape)
-
-'''
 test_set = monai.data.CacheDataset(data[int(len(data) * 0.8):], transform=val_transform) if config_dict['dataset'][
     'cache_dataset'] else monai.data.Dataset(data[int(len(data) * 0.8):], transform=val_transform)
-'''
 
-
-'''
 
 train_loader = monai.data.DataLoader(train_set, batch_size=config_dict['trainer']['batch_size'],
                                      num_workers=config_dict['trainer']['num_workers'], shuffle=True,
@@ -103,5 +84,5 @@ print(f"Test set size: {len(test_set)}")
 
 item = iter(train_loader).__next__()
 print(item["image"].shape, item["mask"].shape)
-'''
+
 
